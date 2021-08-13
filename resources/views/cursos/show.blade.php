e2265eac7172fef09f28be2412b543c30d33b5c7
Genaro10020 <iscgenarovp@gmail.com>
2021-08-13 16:21:49 -0500

    llaves para php
    utilize {{}} para imprimir codigo php

--- resources/views/cursos/show.blade.php
+++ resources/views/cursos/show.blade.php
@@ -1,5 +1,5 @@
 @extends('layouts.plantilla')
 @section('titulo','mostrar')
 @section('content')
-<h1>este es el curso de <?php echo $curso; ?> </h1>
+<h1>este es el curso de {{$curso}} </h1>
 @endsection
